92aa8fb67341db7a747babb5cb895fd21abce554
davidoAraveno <david.aravenap@gmail.com>
2021-10-01 03:21:07 -0300

    input con boton que genera un codigo aleatorio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+
+
 function App() {
+  const [codigoAleatorio, funcionCodigoAleatorio] = useState("")
+  const letras = 'A B C D E F G H I J K L M N O P Q R S T U V';
+  const numeros = '0 1 2 3 4 5 6 7 8 9';
+
+
+  const codeGenerator = (e) => {
+    e.preventDefault();
+
+    const caracteresFinales = letras.split(' ');
+    caracteresFinales.sort(function() {return Math.random() - 0.5})
+    console.log(caracteresFinales)
+  
+    const numerosFinales = numeros.split(' ');
+    numerosFinales.sort(function() {return Math.random() - 0.5});
+    console.log(numerosFinales[0]);
+
+    funcionCodigoAleatorio(caracteresFinales[0]+numerosFinales[0]+caracteresFinales[0]+numerosFinales[0])
+  }
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input type="text" value={codigoAleatorio} />
+        <input type="submit" value="generar codigo" onClick={codeGenerator} />
+      </form>
     </div>
   );
 }
